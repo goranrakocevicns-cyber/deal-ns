edf6a9992164c9018be576f8a6e2a4f8c41221bf
goranrakocevicns-cyber <goranrakocevic.ns@gmail.com>
2026-09-18 17:55:41 +0200

    Add DEAL NS navigation script

---
+++ script.js
@@ -0,0 +1,1 @@
+const header=document.querySelector('.top');document.querySelector('.menu').addEventListener('click',()=>header.classList.toggle('open'));document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>header.classList.remove('open')));
